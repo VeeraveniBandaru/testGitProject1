3077916ab8463d10eb2f7a163dbdb897fff565a1
VeeraveniBandaru <123749256+VeeraveniBandaru@users.noreply.github.com>
2023-03-09 12:14:02 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome from Hello file to gitHub,jenkins and maven.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
